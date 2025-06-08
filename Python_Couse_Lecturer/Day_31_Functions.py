cbced9a0e30a5be87d1b0eed6bc0a982c67d2203
KlivCode <infobrb04@gmail.com>
2025-06-08 21:53:31 +0545

    Learning Pyth

--- Python_Couse_Lecturer/Day_31_Functions.py
+++ Python_Couse_Lecturer/Day_31_Functions.py
@@ -16,3 +16,26 @@ def goodDay(name, ending="Thanks for coming!"):
     print("Good day, " + name + ". " + ending)
 
 goodDay("Bob")
+
+# function with return value
+def add(a, b):
+    return a + b
+
+result = add(5, 3)
+print("The sum is:", result)
+
+# recursion
+
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n* fact(n-1)
+    
+
+def main():
+    number = int(input("Enter a number to find its factorial: "))
+    result = fact(number)
+    print(f"The factorial of {number} is {result}") #f-string for formatting 
+
+main()
