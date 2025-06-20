e72756d3cca51d2d7c777d1ac3a7faf82bea16c1
KlivCode <infobrb04@gmail.com>
2025-06-20 16:09:10 +0545

    PytOOP

---
+++ Python_Couse_Lecturer/_OOP_IN_Python_.py
@@ -0,0 +1,16 @@
+# two approach  1> OOP 2> Functional Programming
+
+'''
+class-> blueprint of an  creating object
+object-> having a particular state and behaviour
+
+'''
+
+class Employee:
+    name="Klivcode"
+    language="Python"
+    salary=100000
+
+
+kliv=Employee()
+print(kliv.name,kliv.language,kliv.salary)
