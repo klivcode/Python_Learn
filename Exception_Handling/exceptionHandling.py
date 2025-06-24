5736eade9fbde4ae31de84666019d47034d029f3
KlivCode <infobrb04@gmail.com>
2025-06-24 14:19:39 +0545

    PYTHON||UPDATING.....

---
+++ Exception_Handling/exceptionHandling.py
@@ -0,0 +1,10 @@
+
+try:
+    a=int(input("Enter a number: "))
+    print(f"You entered: {a}")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+print("Thanks for using the program!")
