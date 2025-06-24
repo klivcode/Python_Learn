73d67ae1cdc1f3266a37fdcf26b167a318e44b38
KlivCode <infobrb04@gmail.com>
2025-06-24 15:27:01 +0545

    Python course is updating again....

--- Advanced_Python/list_Comprehensions.py
+++ Advanced_Python/list_Comprehensions.py
@@ -2,11 +2,11 @@
 
 myList=[1,2,3,4,5,6,7,8,9,10]
 
-# squaredList=[]
-# for item in myList:
-#     squaredList.append(item*item)
+squaredList=[]
+for item in myList:
+    squaredList.append(item*item)
 
-# print(squaredList)
+print(squaredList)
 
 
 # can be done using list comprehensions
